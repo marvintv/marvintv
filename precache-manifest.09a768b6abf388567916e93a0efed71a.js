c190d33fdbeb3f1ace9def0bbc5e6a01c0dc7eaa
Marvin <marvinvilaysack@gmail.com>
2022-12-05 19:44:23 -0600

    Updates

---
+++ precache-manifest.09a768b6abf388567916e93a0efed71a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f57b60324d9dd8486cfc39f28d7271b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18a6c83439fdeadbd191",
+    "url": "/static/css/main.fc2e3a11.chunk.css"
+  },
+  {
+    "revision": "a153c5a409042f4e1d9d",
+    "url": "/static/js/2.0fc7fb4d.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.0fc7fb4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18a6c83439fdeadbd191",
+    "url": "/static/js/main.5e09e275.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "c8814e7b4d9e879935b0dd8268306a0a",
+    "url": "/static/media/marvintv.c8814e7b.jpg"
+  },
+  {
+    "revision": "5bce9ff0d3da8fdabd386a871556be36",
+    "url": "/static/media/resume.5bce9ff0.pdf"
+  }
+]);
